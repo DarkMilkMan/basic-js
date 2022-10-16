6bd708cac0b2f4a6cc552f3594e01c2dfa5c2feb
Dmitry <darkmilkman@gmail.com>
2022-10-16 19:54:14 +0300

    extended-repeater and hanoi-tower succeful

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,10 +15,63 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function repeater(str, options) {
+  let separator = '';
+  let additionSeparator = '';
+  let result = '';
+  let addition = '';
+  str = String(str);
+  
+  if(options.hasOwnProperty('addition')){
+    addition = String(options.addition); 
+    optionAddition = String(options.addition);    
+  }
+ 
+  if(!options.separator){
+    separator = '+';
+  } else {
+    separator = options.separator;
+  }
+
+  if(!options.additionSeparator){
+    additionSeparator = '|';
+  } else {
+    additionSeparator = options.additionSeparator;
+  }
+  
+  if(options.hasOwnProperty('addition')){
+    for (let a = 1;a < options.additionRepeatTimes;a++){    
+        addition += additionSeparator + optionAddition;                     
+    } 
+  }
+
+  if (options.hasOwnProperty('addition') && options.additionRepeatTimes <= 1){
+    result = str + addition;
+  } else if (options.hasOwnProperty('addition') && options.additionRepeatTimes > 1){
+    result = str + addition;
+  } else if (options.hasOwnProperty('addition') && !options.additionRepeatTimes){
+    result = str + addition;
+  } else {
+    result = str;
+  }
+
+  if(options.repeatTimes == 1){
+    console.log(result);
+    return result;
+  } else {
+    for (let i = 1;i < options.repeatTimes;i++) {   
+      if (!options.hasOwnProperty('addition')){
+        result += separator + str; 
+      } else {
+        result += separator + str + addition; 
+      }               
+    }
+  }
+  return result;  
 }
+
+// REPEATABLE_STRINGADDITION|ADDITION|ADDITION+REPEATABLE_STRINGADDITION|ADDITION|ADDITION
 
 module.exports = {
   repeater
